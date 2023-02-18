072243aa8f22a369a155a1f151f463fdea18bec0
I-Maged <magedhanafy@gmail.com>
2023-02-18 22:55:59 +0200

    house model

---
+++ server/models/houseModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const houseSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    location: {
+      type: String,
+      required: [true, 'Please select a location'],
+      enum: ['Cairo', 'Hurghada', 'Sharm El-Sheikh'],
+    },
+    description: {
+      type: String,
+      required: [true, 'Please enter a description of your house'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'Please add price per night'],
+    },
+    discount: {
+      type: Number,
+      required: [true, 'Please add price per night'],
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('House', houseSchema);
